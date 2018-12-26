0ad637d6e7c62c26fc2d4cb24282489f4b06a162
Henrik Holmgren-Jensen <thalmus@gmail.com>
2018-12-26 14:31:11 +0100

    Improved runtime for day3 + solution to part 2

--- WindowsillSoft.AdventOfCode2018.Solutions/Day3/Day3Solver.cs
+++ WindowsillSoft.AdventOfCode2018.Solutions/Day3/Day3Solver.cs
@@ -19,14 +19,7 @@ namespace WindowsillSoft.AdventOfCode2018.Solutions.Day3
             var matcher = new Regex(@"#(?'id'\d+)\s+@\s+(?'x'\d+),(?'y'\d+):\s+(?'width'\d+)x(?'height'\d+)",
                 RegexOptions.Compiled);
 
-            var input = new[]
-            {
-                "#1 @ 1,3: 4x4",
-                "#2 @ 3,1: 4x4",
-                "#3 @ 5,5: 2x2",
-            };
-
-            //var input = File.ReadAllLines("Day3/Day3Input.txt");
+            var input = File.ReadAllLines("Day3/Day3Input.txt");
 
             var claims = input.Select(p => matcher.Match(p))
                 .Select(p => new Claim
@@ -43,24 +36,58 @@ namespace WindowsillSoft.AdventOfCode2018.Solutions.Day3
                 .ThenBy(p => p.Height)
                 .ToArray();
 
-            int minX = claims.Min(p => p.X);
-            int minY = claims.Min(p => p.Y);
-            int maxX = claims.Max(p => p.X + p.Width);
-            int maxY = claims.Max(p => p.Y + p.Height);
-            int count = 0;
-            for (int x = minX; x < maxX; x++)
-                for (int y = minY; y < maxY; y++)
-                {
-                    if (claims.Count(p => p.Contains(x, y)) > 1)
-                        count++;
-                }
-            Console.WriteLine($"{count} overlaps.");
+            //int minX = claims.Min(p => p.X);
+            //int minY = claims.Min(p => p.Y);
+            //int maxX = claims.Max(p => p.X + p.Width);
+            //int maxY = claims.Max(p => p.Y + p.Height);
+            //int count = 0;
+            //for (int x = minX; x < maxX; x++)
+            //    for (int y = minY; y < maxY; y++)
+            //    {
+            //        if (claims.Count(p => p.Contains(x, y)) > 1)
+            //            count++;
+            //    }
+            //Console.WriteLine($"{count} overlaps.");
 
-            for (int i = 0; i < claims.Length - 1; i++)
+            //for (int i = 0; i < claims.Length - 1; i++)
+            //{
+            //    if (!claims.Any(p => p.Id != claims[i].Id && p.Overlaps(claims[i])))
+            //        Console.WriteLine(claims[i].Id);
+            //}
+
+            int[,] reservedAreas = new int[claims.Max(p => p.X + p.Width), claims.Max(p => p.Y + p.Height)];
+
+            HashSet<Claim> nonOverlapping = new HashSet<Claim>(claims);
+            var dicClaims = claims.ToDictionary(p => p.Id);
+
+            int overlaps = 0;
+
+            foreach (var claim in claims)
             {
-                if (!claims.Any(p => p.Id != claims[i].Id && p.Overlaps(claims[i])))
-                    Console.WriteLine(claims[i].Id);
+                for (int x = claim.X; x < claim.X + claim.Width; x++)
+                {
+                    for (int y = claim.Y; y < claim.Y + claim.Height; y++)
+                    {
+                        if (reservedAreas[x, y] != 0)
+                        {
+                            if (nonOverlapping.Contains(claim))
+                                nonOverlapping.Remove(claim);
+                            if (reservedAreas[x, y] != -1)
+                            {
+                                if (nonOverlapping.Contains(dicClaims[reservedAreas[x, y]]))
+                                    nonOverlapping.Remove(dicClaims[reservedAreas[x, y]]);
+
+                                reservedAreas[x, y] = -1;
+                                overlaps++;
+                            }
+                        }
+                        else
+                            reservedAreas[x, y] = claim.Id;
+                    }
+                }
             }
+
+            Console.WriteLine($"After placement, a total of {overlaps} squares had overlaps, and claim id(s) {String.Join(", ", nonOverlapping.Select(p => p.Id))} were not overlapped.");
         }
 
         internal struct Claim
