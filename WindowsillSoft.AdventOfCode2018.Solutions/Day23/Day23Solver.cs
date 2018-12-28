06ecd6fdb28592bf5164081b1745c6187ff2afad
Henrik Holmgren-Jensen <thalmus@gmail.com>
2018-12-28 16:28:35 +0100

    Update Day23 solver with functional part 2 solution

--- WindowsillSoft.AdventOfCode2018.Solutions/Day23/Day23Solver.cs
+++ WindowsillSoft.AdventOfCode2018.Solutions/Day23/Day23Solver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Text.RegularExpressions;
 using WindowsillSoft.AdventOfCode2018.Core;
@@ -18,13 +19,13 @@ namespace WindowsillSoft.AdventOfCode2018.Solutions.Day23
         {
             var matcher = new Regex(@"pos=<(?'x'-?\d+),(?'y'-?\d+),(?'z'-?\d+)>, r=(?'r'\d+)", RegexOptions.Compiled);
 
-            var input = File.ReadAllLines("Day23/Day23Test2.txt")
+            var input = File.ReadAllLines("Day23/Day23Input.txt")
                 .Select(p => matcher.Match(p))
                 .Select(p => new Drone(
-                    long.Parse(p.Groups["x"].Value),
-                    long.Parse(p.Groups["y"].Value),
-                    long.Parse(p.Groups["z"].Value),
-                    long.Parse(p.Groups["r"].Value)))
+                    int.Parse(p.Groups["x"].Value),
+                    int.Parse(p.Groups["y"].Value),
+                    int.Parse(p.Groups["z"].Value),
+                    int.Parse(p.Groups["r"].Value)))
                 .ToList();
 
             var strongestDrone = input.OrderByDescending(p => p.R).First();
@@ -32,165 +33,111 @@ namespace WindowsillSoft.AdventOfCode2018.Solutions.Day23
             Console.WriteLine($"There are {inRangeOfStrongest} drones in range of {strongestDrone}");
 
             var searchspace = new SearchSpace(input);
-            //(xMin: input.Min(p => p.X), xMax: input.Max(p => p.X),
-            //    yMin: input.Min(p => p.Y), yMax: input.Max(p => p.Y),
-            //    zMin: input.Min(p => p.Z), zMax: input.Max(p => p.Z));
-            Console.WriteLine($"Initial best coverage: {searchspace.BestCoverageEstimate()}");
 
             List<SearchSpace> Fringe = new List<SearchSpace>() { searchspace };
-            //var bestSpace = searchspace;
-            (long x, long y, long z) bestPlacement = default;
 
-            //'good' initial coverage guess
-            long bestCoverage = inRangeOfStrongest;
+            (int x, int y, int z) bestPlacement = default;
+            int bestCoverage = 0;
 
             while (Fringe.Any())
             {
                 var candidate = Fringe.First();
                 Fringe.Remove(candidate);
-
                 if (candidate.BestCoverageEstimate() < bestCoverage)
                     continue;
 
-                if (candidate.LargestSize <= 2)
+                if (candidate.Size == 0)
                 {
-                    var coord = candidate.BestCoverageLocation();
-                    var coverage = candidate.GetCoverage(coord);
+                    var coord = candidate.Location;
+                    var coverage = input.Count(p => p.Distance(coord) <= p.R);
                     if (coverage > bestCoverage
                         || coverage == bestCoverage && Length(coord) < Length(bestPlacement))
                     {
                         bestCoverage = coverage;
                         bestPlacement = coord;
-
-                        Console.WriteLine($"Better coord {bestPlacement} found with coverage {bestCoverage}");
+                        Console.CursorLeft = 0;
+                        Console.WriteLine($"Better coord {bestPlacement} found with coverage {bestCoverage} (distance {Length(bestPlacement)})");
                     }
                 }
                 else
                 {
                     foreach (var part in candidate.Partition())
-                        Fringe.Insert(0, part);
-                    //Fringe.AddRange(searchspace.Partition());
+                        Fringe.Add(part);
+                    Fringe = Fringe.OrderByDescending(p => p.BestCoverageEstimate())
+                        .ThenBy(p => Length(p.Location))
+                        .ThenBy(p => p.Size).ToList();
                 }
+
             }
             Console.WriteLine();
-            Console.WriteLine(Length(bestPlacement));
-            //Console.WriteLine($"Initial search space size: {searchspace.LargestSize}");
-            //while (searchspace.LargestSize > 8)
-            //{
-            //    var partitions = searchspace.Partition()
-            //        .Select(p => new { Space = p, Coverage = p.BestCoverageEstimate() }).ToList();
-            //    Console.WriteLine($"  Candiates: {string.Join(", ", partitions.Select(p => p.Space + ": " + p.Coverage))}");
-            //    searchspace = partitions.OrderByDescending(p => p.Coverage)
-            //        .ThenBy(p => p.Space.X.from)
-            //        .ThenBy(p => p.Space.Y.from)
-            //        .ThenBy(p => p.Space.Z.from)
-            //        .First().Space;
-            //    Console.WriteLine($"Partitioned to new space: {searchspace} with best coverage {partitions.OrderByDescending(p => p.Coverage).First().Coverage}");
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine(searchspace.BestCoverageLocation());
+            Console.WriteLine($"Best location found was at {bestPlacement} at distance {Length(bestPlacement)}");
         }
 
-        private long Length((long x, long y, long z) coord)
+        private long Length((int x, int y, int z) coord)
           => Math.Abs(coord.x) + Math.Abs(coord.y) + Math.Abs(coord.z);
     }
 
     public class SearchSpace
     {
-        private List<Drone> _drones;
-        public (long from, long to) X;
-        public (long from, long to) Y;
-        public (long from, long to) Z;
+        private Drone[] _drones;
+        private int _X, _Y, _Z;
+        public int Size { get; }
+        public (int x, int y, int z) Location => (_X, _Y, _Z);
 
         public SearchSpace(List<Drone> drones)
-            : this(drones,
-                  (drones.Min(p => p.X), drones.Max(p => p.X)),
-                  (drones.Min(p => p.Y), drones.Max(p => p.Y)),
-                  (drones.Min(p => p.Z), drones.Max(p => p.Z)))
-        { }
-
-        private SearchSpace(List<Drone> drones, (long from, long to) x, (long from, long to) y, (long from, long to) z)
         {
-            _drones = drones;
-            X = (x.from, Expand(x.to, x.from));
-            Y = (y.from, Expand(y.to, y.from));
-            Z = (z.from, Expand(z.to, z.from));
+            _drones = drones.ToArray();
+            _X = drones.Min(p => p.X);
+            _Y = drones.Min(p => p.Y);
+            _Z = drones.Min(p => p.Z);
+
+            var maxWidth = new[] {
+                drones.Max(p => p.X) - _X,
+                drones.Max(p => p.Y) - _Y,
+                drones.Max(p => p.Z) - _Z }
+                .Max();
+
+            //var size = Math.Pow(2, Math.Ceiling(Math.Log(maxWidth, 2)));
+            //potential overflow due to Double conversion - switching to silly version instead.
+            Size = 1;
+            while (Size < maxWidth) Size <<= 1;
         }
 
-        private long Expand(long to, long from)
+        private SearchSpace(Drone[] drones, int x, int y, int z, int size)
         {
-            return from + (long)Math.Pow(2, Math.Ceiling(Math.Log(to - from, 2)));
+            (_X, _Y, _Z, Size) = (x, y, z, size);
+            _drones = drones.Where(p => p.OverlapsCube(x, y, z, size)).ToArray();
         }
 
-        public long BestCoverageEstimate() =>
-            _drones.Where(p => p.Overlaps(X, Y, Z))
-                //(p.X + p.R >= X.from && X.to >= p.X - p.R) &&
-                //(p.Y + p.R >= Y.from && Y.to >= p.Y - p.R) &&
-                //(p.Z + p.R >= Z.from && Z.to >= p.Z - p.R))
-                .Count();
-
-        public IEnumerable<SearchSpace> Partition()
+        internal int BestCoverageEstimate()
         {
-            if ((X.to - X.from) >= (Y.to - Y.from) && (X.to - X.from) >= (Z.to - Z.from))
-            {
-                var mid = X.from + (long)Math.Floor((X.to - X.from) / 2.0);
-                if (X.from < mid) yield return new SearchSpace(_drones, (X.from, mid), Y, Z);
-                if (X.to > mid) yield return new SearchSpace(_drones, (mid, X.to), Y, Z);
-            }
-            else if (Y.to - Y.from > Z.to - Z.from)
-            {
-                var mid = Y.from + (long)Math.Floor((Y.to - Y.from) / 2.0);
-                if (Y.from < mid) yield return new SearchSpace(_drones, X, (Y.from, mid), Z);
-                if (Y.to > mid) yield return new SearchSpace(_drones, X, (mid, Y.to), Z);
-            }
-            else
-            {
-                var mid = Z.from + (long)Math.Floor((Z.to - Z.from) / 2.0);
-                if (Z.from < mid) yield return new SearchSpace(_drones, X, Y, (Z.from, mid));
-                if (Z.to > mid) yield return new SearchSpace(_drones, X, Y, (mid, Z.to));
-            }
+            return _drones.Count();
         }
 
-        public IEnumerable<(long, long, long)> BuildSet(long from1, long to1, long from2, long to2, long from3, long to3)
-
+        internal IEnumerable<SearchSpace> Partition()
         {
-            for (long v1 = from1; v1 < to1; v1++)
-                for (long v2 = from2; v2 < to2; v2++)
-                    for (long v3 = from3; v3 < to3; v3++)
-                        yield return (v1, v2, v3);
+            yield return new SearchSpace(_drones, _X, _Y, _Z, Size / 2);
+            yield return new SearchSpace(_drones, _X + Size / 2, _Y, _Z, Size / 2);
+            yield return new SearchSpace(_drones, _X, _Y + Size / 2, _Z, Size / 2);
+            yield return new SearchSpace(_drones, _X, _Y, _Z + Size / 2, Size / 2);
+            yield return new SearchSpace(_drones, _X + Size / 2, _Y + Size / 2, _Z, Size / 2);
+            yield return new SearchSpace(_drones, _X + Size / 2, _Y, _Z + Size / 2, Size / 2);
+            yield return new SearchSpace(_drones, _X, _Y + Size / 2, _Z + Size / 2, Size / 2);
+            yield return new SearchSpace(_drones, _X + Size / 2, _Y + Size / 2, _Z + Size / 2, Size / 2);
         }
-        public (long x, long y, long z) BestCoverageLocation()
-        {
-            var res = BuildSet(X.from, X.to, Y.from, Y.to, Z.from, Z.to)
-                .Select(p => new { coord = p, count = GetCoverage(p) })
-                .ToList()
-                .OrderByDescending(p => p.count)
-                .ToList();
-
-            //foreach (var result in res)
-            //    Console.WriteLine($"{result.coord} -> {result.count}");
-
-            return res.First().coord;
-
-        }
-
-        public long LargestSize => Math.Max(Math.Max((X.to - X.from), (Y.to - Y.from)), (Z.to - Z.from));
 
         public override string ToString()
-            => $"({X.from}, {Y.from}, {Z.from}) - ({X.to},{Y.to},{Z.to})";
-
-        internal long GetCoverage((long x, long y, long z) coordinate)
-            => _drones.Count(q => q.Distance(coordinate) <= q.R);
+            => $"{(_X, _Y, _Z)},{Size};{_drones.Length}";
     }
 
     public class Drone
     {
-        public long X { get; }
-        public long Y { get; }
-        public long Z { get; }
-        public long R { get; }
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+        public int R { get; }
 
-        public Drone(long x, long y, long z, long r)
+        public Drone(int x, int y, int z, int r)
         {
             X = x;
             Y = y;
@@ -198,28 +145,27 @@ namespace WindowsillSoft.AdventOfCode2018.Solutions.Day23
             R = r;
         }
 
-        public long Distance(Drone other)
+        public int Distance(Drone other)
             => Distance((other.X, other.Y, other.Z));
 
         public override string ToString()
             => $"<{X},{Y},{Z}>,R{R}";
 
-        internal long Distance((long x, long y, long z) other)
+        public int Distance((int x, int y, int z) other)
             => Math.Abs(other.x - X)
                 + Math.Abs(other.y - Y)
                 + Math.Abs(other.z - Z);
 
-        internal bool Overlaps((long from, long to) x, (long from, long to) y, (long from, long to) z)
+        internal bool OverlapsCube(int x, int y, int z, int size)
         {
-            if (X + R < x.from) return false;
-            if (X - R > x.to) return false;
-
-            if (Y + R < y.from) return false;
-            if (Y - R > y.to) return false;
-
-            if (Z + R < z.from) return false;
-            if (Z - R > z.to) return false;
-            return true;
+            var d = 0;
+            if (X > x + size) d += X - (x + size);
+            if (X < x) d += x - X;
+            if (Y > y + size) d += Y - (y + size);
+            if (Y < y) d += y - Y;
+            if (Z > z + size) d += Z - (z + size);
+            if (Z < z) d += z - Z;
+            return d <= R;
         }
     }
 }
