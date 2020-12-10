8046a7d38d989095713908309fa03bb537bc27e4
=Henrik Holmgren-Jensen <=>
2020-12-10 21:26:02 +0100

    Add solvers for AoC 2020 days 8, 9.

---
+++ WindowsillSoft.CodeChallenges.AdventOfCode/2020/Day08.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WindowsillSoft.CodeChallenges.Core;
+
+namespace WindowsillSoft.CodeChallenges.AdventOfCode._2020
+{
+    public class Day08 : AdventOfCode2020SolverBase
+    {
+        protected record Statement(string Instruction, int Parameter);
+        protected record State(int Accumulator, int ProgramCounter);
+
+        List<Statement> _program = new();
+        protected Dictionary<string, Func<State, int, State>> _instructions = new Dictionary<string, Func<State, int, State>>
+        {
+            ["nop"] = (state, param) => state with { ProgramCounter = state.ProgramCounter + 1 },
+            ["acc"] = (state, param) => state with { Accumulator = state.Accumulator + param, ProgramCounter = state.ProgramCounter + 1 },
+            ["jmp"] = (state, param) => state with { ProgramCounter = state.ProgramCounter + param },
+        };
+
+        public Day08(IIOProvider provider) : base(provider) { }
+
+        public override string Name => "Day 8: Handheld Halting";
+        public override string ExecutePart1() => RunProgram(_program).Accumulator.ToString();
+        public override string ExecutePart2()
+        {
+            var copy = _program.ToList();
+
+            for (int i = 0; i < copy.Count; i++)
+            {
+                if (copy[i].Instruction == "acc") continue;
+                var tmp = copy[i];
+                if (copy[i].Instruction == "nop")
+                    copy[i] = copy[i] with { Instruction = "jmp" };
+                else
+                    copy[i] = copy[i] with { Instruction = "nop" };
+
+                var finalState = RunProgram(copy);
+                if (finalState.ProgramCounter == copy.Count)
+                    return finalState.Accumulator.ToString();
+
+                copy[i] = tmp;
+            }
+            return "No final state reached";
+        }
+
+        private State RunProgram(List<Statement> program)
+        {
+            HashSet<int> visitedInstructions = new();
+            var state = new State(0, 0);
+            while (true)
+            {
+                visitedInstructions.Add(state.ProgramCounter);
+
+                var instruction = program[state.ProgramCounter];
+                var newState = _instructions[instruction.Instruction](state, instruction.Parameter);
+                if (visitedInstructions.Contains(newState.ProgramCounter) || newState.ProgramCounter >= program.Count)
+                    return newState;
+
+                if (visitedInstructions.Contains(newState.ProgramCounter))
+                    return state;
+                if (newState.ProgramCounter >= program.Count)
+                    return state;
+                state = newState;
+            }
+        }
+
+        public override void Initialize(string input)
+        {
+            _program = ReadAndSplitInput<Statement>(input,
+                p => new Statement(p[..3], int.Parse(p[4..])), '\r', '\n')
+                .ToList();
+        }
+    }
+}
---
+++ WindowsillSoft.CodeChallenges.AdventOfCode/2020/Day09.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using WindowsillSoft.CodeChallenges.Core;
+
+namespace WindowsillSoft.CodeChallenges.AdventOfCode._2020
+{
+    public class Day09 : AdventOfCode2020SolverBase
+    {
+        private int _preampleLength;
+        private long[] _code;
+
+        public Day09(IIOProvider provider) : base(provider) { _code = new long[0]; }
+
+        public override string Name => "Day 9: Encoding Error";
+
+        //Naïve version for now, there is probably good optimization to be had by keeping state.
+        public override string ExecutePart1()
+        {
+            var index = FindFirstBrokenIndex();
+            if (index > 0)
+                return _code[index].ToString();
+            return "No solution found.";
+        }
+
+        public override string ExecutePart2()
+        {
+            var brokenIndex = FindFirstBrokenIndex();
+            for (int i = 0; i < brokenIndex; i++)
+            {
+                for (int j = i; j < brokenIndex; j++)
+                {
+                    if (_code[i..j].Sum() == _code[brokenIndex])
+                        return (_code[i..j].Min() + _code[i..j].Max()).ToString();
+                }
+            }
+            return "No solution found.";
+        }
+
+        private int FindFirstBrokenIndex()
+        {
+            for (int i = _preampleLength; i < _code.Length; i++)
+            {
+                if (!_code[(i - _preampleLength)..i].Any(p => _code[(i - _preampleLength)..i].Where(q => p != q).Select(q => p + q).Any(p => p == _code[i])))
+                    return i;
+            }
+            return -1;
+        }
+
+        public override void Initialize(string input)
+        {
+            var raw = ReadAndSplitInput<long>(input);
+            _preampleLength = (int)raw.First();
+            _code = raw.Skip(1).ToArray();
+        }
+
+    }
+}
