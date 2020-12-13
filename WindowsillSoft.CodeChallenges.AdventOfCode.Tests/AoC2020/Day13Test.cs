7337d421b4609400c9e9810e930c87e3a3c4f00e
=Henrik Holmgren-Jensen <=>
2020-12-13 15:21:56 +0100

    Add AoC 2020 day 13 solver

---
+++ WindowsillSoft.CodeChallenges.AdventOfCode.Tests/AoC2020/Day13Test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using WindowsillSoft.CodeChallenges.AdventOfCode._2020;
+
+namespace WindowsillSoft.CodeChallenges.AdventOfCode.Tests.AoC2020
+{
+    public class Day13Test : TestBase<Day13>
+    {
+        [Test,
+            TestCase("939\n7,13,x,x,59,x,31,19", "295")]
+        public void Part1(string input, string result)
+        {
+            var solver = GetSolver(input);
+
+            Assert.That(solver.ExecutePart1(), Is.EqualTo(result));
+        }
+
+        [Test,
+            TestCase("939\n7,13,x,x,59,x,31,19", "1068781"),
+            TestCase("1\n17,-1,13,19", "3417"),
+            TestCase("1\n67,7,59,61", "754018"),
+            TestCase("1\n67,-1,7,59,61", "779210"),
+            TestCase("1\n67,7,-1,59,61", "1261476"),
+            TestCase("1\n1789,37,47,1889", "1202161486")]
+        public void Part2(string input, string result)
+        {
+            var solver = GetSolver(input);
+
+            Assert.That(solver.ExecutePart2(), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void FullRun()
+        {
+            var solver = GetSolver("1000067\n17,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,439,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,x,x,x,x,x,23,x,x,x,x,x,x,x,787,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,19");
+            Assert.That(solver.ExecutePart1(), Is.EqualTo("205"));
+            Assert.That(solver.ExecutePart2(), Is.EqualTo("803025030761664"));
+        }
+    }
+}
---
+++ WindowsillSoft.CodeChallenges.AdventOfCode/2020/Day13.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using WindowsillSoft.CodeChallenges.Core;
+
+namespace WindowsillSoft.CodeChallenges.AdventOfCode._2020
+{
+    public class Day13 : AdventOfCode2020SolverBase
+    {
+        private int _departureTime;
+        private int[] _busses = new int[0];
+
+        public Day13(IIOProvider provider) : base(provider) { }
+
+        public override string Name => "Day 13: Shuttle Search";
+        public override string ExecutePart1()
+        {
+            var bestBus = _busses
+                .Where(p => p != -1)
+                .OrderBy(p => p - _departureTime % p)
+                .First();
+            return (bestBus * (bestBus - _departureTime % bestBus)).ToString();
+        }
+
+        public override string ExecutePart2()
+        {
+            long increment = _busses[0];
+            long result = 0;
+
+            for (int i = 1; i < _busses.Length; i++)
+            {
+                if (_busses[i] == -1) continue;
+
+                while ((result + i) % _busses[i] != 0)
+                    result += increment;
+
+                increment *= _busses[i];
+            }
+            return result.ToString();
+        }
+
+        public override void Initialize(string input)
+        {
+            var split = input.Split('\r', '\n');
+            _departureTime = int.Parse(split[0]);
+            _busses = split[1].Split(',')
+                .Select(p => p == "x" ? -1 : int.Parse(p))
+                .ToArray();
+        }
+    }
+}
