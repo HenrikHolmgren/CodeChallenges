57b26a631aeb70c21f376923a6a2cb02c5e56e76
=Henrik Holmgren-Jensen <=>
2022-12-05 12:02:41 +0100

    Add solvers for 2022 days 4 and 5

---
+++ Advent of Code/2022/Day04/Program.cs
@@ -0,0 +1,28 @@
+﻿using var client = new AoC.Api.AoCClient();
+
+var input = await client.GetParsedAsync(2022, 4, line =>
+{
+    var sections = line.Split(',');
+    return new Assignment(Section.Parse(sections[0]), Section.Parse(sections[1]));
+});
+
+Console.WriteLine("Part 1: " + input.Count(p => p.HasFullOverlap));
+Console.WriteLine("Part 2: " + input.Count(p => p.WastesWork));
+
+record Assignment(Section A, Section B)
+{
+    public bool HasFullOverlap => A.Contains(B) || B.Contains(A);
+    public bool WastesWork => HasFullOverlap || A.OverlapsRight(B) || B.OverlapsRight(A);
+}
+
+record Section(int From, int To)
+{
+    public static Section Parse(string val)
+    {
+        var boundaries = val.Split("-").Select(Int32.Parse).ToArray();
+        return new(boundaries[0], boundaries[1]);
+    }
+
+    public bool Contains(Section other) => From <= other.From && To >= other.To;
+    public bool OverlapsRight(Section other) => From <= other.From && other.From <= To;
+}
---
+++ Advent of Code/2022/Day05/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+
+using var client = new AoC.Api.AoCClient();
+
+var input = await client.GetRawInputAsync(2022, 05);
+
+List<Stack<char>> State = new();
+var initialStateInput = input.Split("\n\n")[0]
+    .Split('\n', StringSplitOptions.RemoveEmptyEntries).Reverse().ToArray();
+int columnCount = initialStateInput[0].Length / 4 + 1;
+for (int i = 0; i < columnCount ; i++)
+{
+    State.Add(new());
+    State.Add(new());
+}
+
+foreach (var line in initialStateInput.Skip(1))
+{
+    foreach (var entry in line.Chunk(4).Select((p, i) => (Label: p.Skip(1).First(), Column: i)))
+    {
+        if (entry.Label != ' ')
+        {
+            State[entry.Column].Push(entry.Label);
+            State[entry.Column + columnCount].Push(entry.Label);
+        }
+    }
+}
+
+var commands = input.Split("\n\n")[1].Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(MoveCommand.Parse).ToArray();
+
+Stack<char> scratch = new();
+foreach(var command in commands)
+{
+    for (int i = 0; i < command.Count; i++)
+    {
+        State[command.To].Push(State[command.From].Pop());
+        scratch.Push(State[command.From + columnCount].Pop());
+    }
+
+    for (int i = 0; i < command.Count; i++)
+        State[command.To+columnCount].Push(scratch.Pop());
+}
+
+Console.WriteLine("Part 1: " + string.Join("", State.Take(columnCount).Select(p=>p.Peek())));
+Console.WriteLine("Part 2: " + string.Join("", State.Skip(columnCount).Select(p=>p.Peek())));
+
+record MoveCommand(int Count, int From, int To)
+{
+    private static Regex _parser = new Regex(@"move (\d+) from (\d+) to (\d+)");
+    public static MoveCommand Parse(string commandText)
+    {
+        var match = _parser.Match(commandText);
+        if (!match.Success) throw new FormatException(commandText + " does not match expected pattern.");
+        return new(
+            Int32.Parse(match.Groups[1].Value),
+            Int32.Parse(match.Groups[2].Value) - 1,
+            Int32.Parse(match.Groups[3].Value) - 1);
+    }
+}
